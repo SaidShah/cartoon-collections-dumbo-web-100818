b180b3b33b0c37c51ce2bc5417284ff6628e9e8a
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 15:45:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(given_array)
+  given_array.each do |each_dwarf|
+    puts each_dwarf
+  end
 end
 
 def summon_captain_planet# code an argument here
