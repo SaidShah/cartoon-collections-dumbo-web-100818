a96067bf37276bc9066912b7067d2c265727e643
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 17:09:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ def find_the_cheese(given_array)
   # the array below is here to help
   index=nil
   cheese_types = ["cheddar", "gouda", "camembert"]
-  given_array.each do | each_element |
- index =  cheese_types.find_index(each_element)
+  given_array.collect do | each_element |
+  cheese_types.select(each_element)
 end
-  given_array[index]
+  
 end
